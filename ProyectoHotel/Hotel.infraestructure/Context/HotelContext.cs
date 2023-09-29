905fa02310804224664a254aaf51c7a4b669e3aa
Daimon Aquino <daimonaquino0228@gmail.com>
2023-09-29 01:50:29 -0400

    Avance del primer video sin conectar con los id

---
+++ ProyectoHotel/Hotel.infraestructure/Context/HotelContext.cs
@@ -0,0 +1,18 @@
+﻿using Hotel.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.infraestructure.Context
+{
+    public class HotelContext : DbContext
+    {
+        public HotelContext(DbContextOptions<HotelContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Reception> Receptions { get; set; }
+    }
+}
---
+++ ProyectoHotel/Hotel.infraestructure/Interfaces/IReceptionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Hotel.domain.Entities;
+using Hotel.domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.infraestructure.Interfaces
+{
+    public interface IReceptionRepository : IBaseRepository<Reception>
+    {
+    }
+}
