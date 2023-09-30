d5f5658c557e161670e299c2a18af0b53cc44706
Daimon Aquino <daimonaquino0228@gmail.com>
2023-09-29 23:30:54 -0400

    Movi el documento que tenia en models que iba en repositories

---
+++ ProyectoHotel/Hotel.infraestructure/Repositories/ReceptionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Hotel.domain.Entities;
+using Hotel.infraestructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.infraestructure.Repositories
+{
+    public class ReceptionRepository : IReceptionRepository
+    {
+        public List<Reception> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reception GetEntity(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Reception entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Reception entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Reception entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
