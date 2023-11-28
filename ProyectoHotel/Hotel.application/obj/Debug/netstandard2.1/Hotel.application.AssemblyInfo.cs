d9651103d355506e3b125229e4fc2c47576b663e
AnderDev009 <richardanderdev009@gmail.com>
2023-11-28 17:56:07 -0400

    Referencia al proyecto web

--- ProyectoHotel/Hotel.application/obj/Debug/netstandard2.1/Hotel.application.AssemblyInfo.cs
+++ ProyectoHotel/Hotel.application/obj/Debug/netstandard2.1/Hotel.application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel.application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb61fa957fd9bf28cba29faf99635b2729b80db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel.application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel.application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProyectoHotel/hotel.api2/obj/Debug/net8.0/hotel.api.AssemblyInfo.cs
+++ ProyectoHotel/hotel.api2/obj/Debug/net8.0/hotel.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotel.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+811b1755e1ff0ad88b6cde884d80f8cfbceb8011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb61fa957fd9bf28cba29faf99635b2729b80db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotel.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotel.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
