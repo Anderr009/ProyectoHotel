9ab40f76b9938f23c23fbb554e5c2a3a8d436713
UriAbreuGon <99096669+UriAbreuGon@users.noreply.github.com>
2023-12-09 21:07:04 -0400

    Update ClientDetailResponse.cs

--- ProyectoHotel/Hotel.web/Models/Response/ClientDetailResponse.cs
+++ ProyectoHotel/Hotel.web/Models/Response/ClientDetailResponse.cs
@@ -4,6 +4,6 @@
     {
         public bool success { get; set; }
         public object message { get; set; }
-        public RoomViewModel data { get; set; }
+        public ClientViewModel data { get; set; }
     }
 }
